bff3ee7362e0b1d9b3b00aa0f0bd9361605a8970
David Ilenwabor <dave11ilen@gmail.com>
2020-06-28 20:12:43 +0100

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by David Ilenwabor on 28/06/2020.
+//  Copyright © 2020 TQG Digital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
